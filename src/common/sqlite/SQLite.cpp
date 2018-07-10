4c6a6331074f722b262500f237a3382c84cacb4c
midikit <midikit@naver.com>
2018-07-10 13:03:53 +0900

    Add files via upload

---
+++ src/common/sqlite/SQLite.cpp
@@ -0,0 +1,379 @@
+#include "SQLite.h"
+#include "../../graysvr/graysvr.h"
+
+CSQLite::CSQLite()
+{
+	m_socket = NULL;
+	m_resultCode = SQLITE_OK;
+}
+
+CSQLite::~CSQLite()
+{
+	if ( m_socket )
+		Close();
+}
+
+void CSQLite::Connect(LPCTSTR pszFileName)
+{
+	ADDTOCALLSTACK("CSQLite::Connect");
+	if ( m_socket )
+		return;
+
+	m_resultCode = sqlite3_open(UTF8MBSTR(pszFileName), &m_socket);
+	if ( m_resultCode != SQLITE_OK )
+	{
+		g_Log.EventError("SQLite error #%d: %s\n", m_resultCode, sqlite3_errmsg(m_socket));
+		sqlite3_close(m_socket);
+	}
+}
+
+void CSQLite::Close()
+{
+	ADDTOCALLSTACK("CSQLite::Close");
+	sqlite3_close(m_socket);
+	m_socket = NULL;
+}
+
+TablePtr CSQLite::QueryPtr(LPCTSTR pszQuery)
+{
+	ADDTOCALLSTACK("CSQLite::QueryPtr");
+	char **retStrings = 0;
+	char *errmsg = 0;
+	int iRows = 0, iCols = 0;
+
+	m_resultCode = sqlite3_get_table(m_socket, UTF8MBSTR(pszQuery), &retStrings, &iRows, &iCols, &errmsg);
+	if ( m_resultCode != SQLITE_OK )
+	{
+		g_Log.EventError("SQLite error #%d: %s [Cmd: \"%s\"]\n", m_resultCode, errmsg, pszQuery);
+		sqlite3_free(errmsg);
+	}
+
+	Table *pTable = new Table();
+	pTable->m_iCols = iCols;
+	pTable->m_iRows = iRows;
+	if ( iRows > 0 )
+		pTable->m_iPos = 0;
+	pTable->m_strlstCols.reserve(iCols);
+
+	int iPos = 0;
+	for ( ; iPos < iCols; ++iPos )
+	{
+		pTable->m_strlstCols.push_back(stdvstring());
+
+		if ( retStrings[iPos] )
+			ConvertUTF8ToString(retStrings[iPos], pTable->m_strlstCols.back());
+		else
+			pTable->m_strlstCols.back().push_back('\0');
+	}
+
+	pTable->m_lstRows.resize(iRows);
+	for ( int iRow = 0; iRow < iRows; ++iRow )
+	{
+		pTable->m_lstRows[iRow].reserve(iCols);
+		for ( int iCol = 0; iCol < iCols; ++iCol, ++iPos )
+		{
+			pTable->m_lstRows[iRow].push_back(stdvstring());
+
+			if ( retStrings[iPos] )
+				ConvertUTF8ToString(retStrings[iPos], pTable->m_lstRows[iRow].back());
+			else
+				pTable->m_lstRows[iRow].back().push_back('\0');
+		}
+	}
+
+	sqlite3_free_table(retStrings);
+	return TablePtr(pTable);
+}
+
+void CSQLite::Query(LPCTSTR pszQuery, CVarDefMap &mapQueryResult)
+{
+	ADDTOCALLSTACK("CSQLite::Query");
+	mapQueryResult.Empty();
+	mapQueryResult.SetNumNew("NUMROWS", 0);
+
+	if ( !m_socket )
+		return;
+
+	TablePtr pTable = QueryPtr(pszQuery);
+	if ( pTable.m_pTable->m_iRows )
+	{
+		int iRows = pTable.m_pTable->m_iRows;
+		int iCols = pTable.m_pTable->m_iCols;
+		mapQueryResult.SetNum("NUMROWS", iRows);
+		mapQueryResult.SetNum("NUMCOLS", iCols);
+
+		char *pszKey = Str_GetTemp();
+		for ( int iRow = 0; iRow < iRows; ++iRow )
+		{
+			pTable.m_pTable->GoRow(iRow);
+			for ( int iCol = 0; iCol < iCols; ++iCol )
+			{
+				sprintf(pszKey, "%d.%d", iRow, iCol);
+				mapQueryResult.SetStr(pszKey, true, pTable.m_pTable->GetColValue(iCol));
+				sprintf(pszKey, "%d.%s", iRow, pTable.m_pTable->GetColName(iCol));
+				mapQueryResult.SetStr(pszKey, true, pTable.m_pTable->GetColValue(iCol));
+			}
+		}
+	}
+}
+
+void CSQLite::Exec(LPCTSTR pszQuery)
+{
+	ADDTOCALLSTACK("CSQLite::Exec");
+	if ( !m_socket )
+		return;
+
+	char *errmsg = 0;
+
+	m_resultCode = sqlite3_exec(m_socket, UTF8MBSTR(pszQuery), 0, 0, &errmsg);
+	if ( m_resultCode != SQLITE_OK )
+	{
+		g_Log.EventError("SQLite error #%d: %s [Cmd: \"%s\"]\n", m_resultCode, errmsg, pszQuery);
+		sqlite3_free(errmsg);
+	}
+}
+
+void CSQLite::ConvertUTF8ToString(LPTSTR pszIn, stdvstring &pszOut)
+{
+	ADDTOCALLSTACK("CSQLite::ConvertUTF8ToString");
+	size_t len = strlen(pszIn) + 1;
+	pszOut.resize(len, 0);
+
+	wchar_t *wChar = new wchar_t[len];
+	wChar[0] = 0;
+
+	mbstowcs(wChar, pszIn, len);
+	wcstombs(&pszOut[0], wChar, len);
+
+	delete[] wChar;
+}
+
+enum LDBO_TYPE
+{
+	LDBO_CONNECTED,
+	LDBO_ROW,
+	LDBO_QTY
+};
+
+LPCTSTR const CSQLite::sm_szLoadKeys[LDBO_QTY + 1] =
+{
+	"CONNECTED",
+	"ROW",
+	NULL
+};
+
+enum LDBOV_TYPE
+{
+	LDBOV_CLOSE,
+	LDBOV_CONNECT,
+	LDBOV_EXECUTE,
+	LDBOV_QUERY,
+	LDBOV_QTY
+};
+
+LPCTSTR const CSQLite::sm_szVerbKeys[LDBOV_QTY + 1] =
+{
+	"CLOSE",
+	"CONNECT",
+	"EXECUTE",
+	"QUERY",
+	NULL
+};
+
+bool CSQLite::r_GetRef(LPCTSTR &pszKey, CScriptObj *&pRef)
+{
+	ADDTOCALLSTACK("CSQLite::r_GetRef");
+	UNREFERENCED_PARAMETER(pszKey);
+	UNREFERENCED_PARAMETER(pRef);
+	return false;
+}
+
+bool CSQLite::r_LoadVal(CScript &s)
+{
+	ADDTOCALLSTACK("CSQLite::r_LoadVal");
+	UNREFERENCED_PARAMETER(s);
+	return false;
+}
+
+bool CSQLite::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
+{
+	ADDTOCALLSTACK("CSQLite::r_WriteVal");
+	EXC_TRY("WriteVal");
+
+	int index = FindTableHeadSorted(pszKey, sm_szLoadKeys, COUNTOF(sm_szLoadKeys) - 1);
+	switch ( index )
+	{
+		case LDBO_CONNECTED:
+			sVal.FormatVal(m_socket != NULL);
+			break;
+
+		case LDBO_ROW:
+			pszKey += strlen(sm_szLoadKeys[index]);
+			SKIP_SEPARATORS(pszKey);
+			sVal = m_QueryResult.GetKeyStr(pszKey);
+			break;
+
+		default:
+			return false;
+	}
+
+	return true;
+	EXC_CATCH;
+
+	EXC_DEBUG_START;
+	g_Log.EventDebug("command '%s' [%p]\n", pszKey, static_cast<void *>(pSrc));
+	EXC_DEBUG_END;
+	return false;
+}
+
+bool CSQLite::r_Verb(CScript &s, CTextConsole *pSrc)
+{
+	ADDTOCALLSTACK("CSQLite::r_Verb");
+	EXC_TRY("Verb");
+
+	int index = FindTableSorted(s.GetKey(), sm_szVerbKeys, COUNTOF(sm_szVerbKeys) - 1);
+	switch ( index )
+	{
+		case LDBOV_CLOSE:
+			if ( m_socket )
+				Close();
+			break;
+
+		case LDBOV_CONNECT:
+			if ( !m_socket )
+				Connect(s.GetArgRaw());
+			break;
+
+		case LDBOV_EXECUTE:
+			Exec(s.GetArgRaw());
+			break;
+
+		case LDBOV_QUERY:
+			Query(s.GetArgRaw(), m_QueryResult);
+			break;
+
+		default:
+			return false;
+	}
+
+	return true;
+	EXC_CATCH;
+
+	EXC_DEBUG_START;
+	g_Log.EventDebug("command '%s' args '%s' [%p]\n", s.GetKey(), s.GetArgRaw(), static_cast<void *>(pSrc));
+	EXC_DEBUG_END;
+	return false;
+}
+
+///////////////////////////////////////////////////////////
+// Table
+
+bool Table::GoRow(int iRow)
+{
+	ADDTOCALLSTACK("Table::GoRow");
+	if ( iRow < static_cast<int>(m_lstRows.size()) )
+	{
+		m_iPos = iRow;
+		return true;
+	}
+	return false;
+}
+
+LPCTSTR Table::GetColName(int iCol)
+{
+	ADDTOCALLSTACK("Table::GetColName");
+	if ( (iCol >= 0) && (iCol < m_iCols) )
+		return &m_strlstCols[iCol][0];
+	return 0;
+}
+
+LPCTSTR Table::GetColValue(int iCol)
+{
+	ADDTOCALLSTACK("Table::GetColValue");
+	if ( (iCol >= 0) && (iCol < m_iCols) && (m_iPos >= 0) )
+		return &m_lstRows[m_iPos][iCol][0];
+	return 0;
+}
+
+///////////////////////////////////////////////////////////
+// TablePtr
+
+TablePtr::TablePtr()
+{
+	m_pTable = NULL;
+}
+
+TablePtr::TablePtr(Table *pTable)
+{
+	m_pTable = pTable;
+}
+
+TablePtr::~TablePtr()
+{
+	if ( m_pTable )
+		delete m_pTable;
+}
+
+///////////////////////////////////////////////////////////
+// UTF8MBSTR
+
+UTF8MBSTR::UTF8MBSTR(LPCTSTR pszArgs)
+{
+	if ( pszArgs )
+		m_iLen = ConvertStringToUTF8(pszArgs, m_pszUTF8_MultiByte);
+	else
+	{
+		m_pszUTF8_MultiByte = new char[1];
+		m_pszUTF8_MultiByte[0] = 0;
+		m_iLen = 0;
+	}
+}
+
+UTF8MBSTR::UTF8MBSTR(UTF8MBSTR &pszArgs)
+{
+	m_iLen = pszArgs.m_iLen;
+	m_pszUTF8_MultiByte = new char[m_iLen + 1];
+	strncpy(m_pszUTF8_MultiByte, pszArgs.m_pszUTF8_MultiByte, m_iLen + 1);
+}
+
+UTF8MBSTR::~UTF8MBSTR()
+{
+	if ( m_pszUTF8_MultiByte )
+		delete[] m_pszUTF8_MultiByte;
+}
+
+size_t UTF8MBSTR::ConvertStringToUTF8(LPCTSTR pszIn, char *&pszOut)
+{
+	ADDTOCALLSTACK("UTF8MBSTR::ConvertStringToUTF8");
+	size_t len = strlen(pszIn);
+	wchar_t *wChar = new wchar_t[len + 1];
+	wChar[0] = 0;
+	mbstowcs(wChar, pszIn, len + 1);
+
+	size_t lenRequired = wcstombs(NULL, wChar, len + 1);
+	pszOut = new char[lenRequired + 1];
+	pszOut[0] = 0;
+	wcstombs(pszOut, wChar, lenRequired + 1);
+
+	delete[] wChar;
+	return len;
+}
+
+void UTF8MBSTR::ConvertUTF8ToString(LPCTSTR pszIn, char *&pszOut, size_t len)
+{
+	ADDTOCALLSTACK("UTF8MBSTR::ConvertUTF8ToString");
+	wchar_t *wChar = new wchar_t[len];
+	wChar[0] = 0;
+	mbstowcs(wChar, pszIn, len);
+
+	pszOut = new char[len];
+	pszOut[0] = 0;
+	wcstombs(pszOut, wChar, len);
+
+	delete[] wChar;
+}
+
+UTF8MBSTR::operator char *()
+{
+	return m_pszUTF8_MultiByte;
+}
---
+++ src/common/sqlite/SQLite.h
@@ -0,0 +1,111 @@
+#ifndef _INC_SQLITE_H
+#define _INC_SQLITE_H
+#pragma once
+
+#include "../graycom.h"
+#include "sqlite3.h"
+
+typedef std::vector<TCHAR> stdvstring;
+typedef std::vector<stdvstring> vstrlist;
+typedef vstrlist row;
+
+class TablePtr;
+
+class CSQLite : public CScriptObj
+{
+public:
+	static const char *m_sClassName;
+	static LPCTSTR const sm_szLoadKeys[];
+	static LPCTSTR const sm_szVerbKeys[];
+
+	CSQLite();
+	~CSQLite();
+
+private:
+	sqlite3 *m_socket;
+	int m_resultCode;
+
+public:
+	CVarDefMap m_QueryResult;
+
+public:
+	void Connect(LPCTSTR pszFileName);
+	void Close();
+
+	TablePtr QueryPtr(LPCTSTR pszQuery);
+	void Query(LPCTSTR pszQuery, CVarDefMap &mapQueryResult);
+	void Exec(LPCTSTR pszQuery);
+
+	virtual bool r_GetRef(LPCTSTR &pszKey, CScriptObj *&pRef);
+	virtual bool r_LoadVal(CScript &s);
+	virtual bool r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc);
+	virtual bool r_Verb(CScript &s, CTextConsole *pSrc);
+
+	LPCTSTR GetName() const
+	{
+		return "SQLite_OBJ";
+	}
+
+private:
+	void ConvertUTF8ToString(LPTSTR pszIn, stdvstring &pszOut);
+};
+
+class Table
+{
+	friend class CSQLite;
+
+public:
+	Table(void)
+	{
+		m_iRows = m_iCols = 0;
+		m_iPos = -1;
+	};
+	virtual ~Table() { };
+
+private:
+	int m_iRows;
+	int m_iCols;
+	int m_iPos;
+
+	row m_strlstCols;
+	std::vector<row> m_lstRows;
+
+public:
+	bool GoRow(int iRow);
+	LPCTSTR GetColName(int iCol);
+	LPCTSTR GetColValue(int iCol);
+};
+
+// Class to contain a pointer to a Table class, which will spare the user from freeing a pointer
+class TablePtr
+{
+public:
+	TablePtr();
+	TablePtr(Table *pTable);
+	virtual ~TablePtr();
+
+public:
+	Table *m_pTable;
+};
+
+// Class for converting TCHAR to multi-byte UTF-8 and vice versa
+class UTF8MBSTR
+{
+public:
+	UTF8MBSTR(LPCTSTR pszArgs);
+	UTF8MBSTR(UTF8MBSTR &pszArgs);
+	virtual ~UTF8MBSTR();
+
+private:
+	size_t m_iLen;
+	char *m_pszUTF8_MultiByte;
+
+private:
+	size_t ConvertStringToUTF8(LPCTSTR pszIn, char *&pszOut);
+	static void ConvertUTF8ToString(LPCTSTR pszIn, char *&pszOut, size_t len);
+
+public:
+	operator char *();
+};
+
+#endif	// _INC_SQLITE_H
